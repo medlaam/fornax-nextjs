61abffad9207b48c7af9a10b07273093b9f58643
Rakibur Rahman <rakibur5454@gmail.com>
2021-11-18 16:13:50 +0600

    complete routing for pagination

--- components/pagination.js
+++ components/pagination.js
@@ -1,35 +1,41 @@
+import { useRouter } from 'next/router';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import styles from '../styles/pagination.module.css';
 
-const Pagination = ({ postPerPage, totalPost, paginate }) => {
+const Pagination = ({ postPerPage, totalPost, page, paginate, hasNextPage, hasPreviousPage }) => {
   const pageNumber = [];
 
   for (let i = 1; i <= Math.ceil(totalPost / postPerPage); i++) {
     pageNumber.push(i)
   }
 
+  const router = useRouter();
+
+
   return (
     <div className="p-4 mb-5">
       <ul className={`flex justify-between ${styles.pagination}`}>
-        <li className={`px-5 h-10 ${styles.page}`}>
-          <a className={styles.arrow} href="#"><FaAngleLeft /></a>
+        <li onClick={() => router.push(`/?page=${page - 1}`)} className={`px-5 h-10 ${styles.page}`}>
+          <a className={styles.arrow}><FaAngleLeft /></a>
         </li>
         <li>
           <ul className={`flex flex-wrap ${styles.pagination}`}>
             {
               pageNumber.map(num => (
-                <li key={num} className={`px-5 h-10 ${styles.page} `}><a  onClick={() => paginate(num)}  href="#">{num}</a>
+                <li key={num} className={`px-5 h-10 ${styles.page} `}><a onClick={() => {paginate(num); router.push(`/?page=${num}`)}}>{num}</a>
                 </li>
               ))
             }
           </ul>
         </li>
-        <li className={`px-5 h-10 ${styles.page}`}>
-          <a className={styles.arrow} href="#"><FaAngleRight /></a>
+        <li disabled={hasNextPage} onClick={() => router.push(`/?page=${page + 1}`)} className={`px-5 h-10 ${styles.page}`}>
+          <a className={styles.arrow}><FaAngleRight /></a>
         </li>
       </ul>
     </div>
   );
 };
 
+
 export default Pagination;
+
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,22 @@ import Blogs from '../components/blogs'
 import Pagination from '../components/pagination';
 import Head from 'next/head';
 import { useState } from 'react';
-import {blogData} from '../components/blogData';
+import { blogData } from '../components/blogData';
 
-export default function Home() {
+function Home({ posts, page }) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(3);
+  const [postPerPage] = useState(2);
 
-  const indexOfLastPost = currentPage * postPerPage
+  const indexOfLastPost = page * postPerPage
   const indexOfFirstPost = indexOfLastPost - postPerPage
-  const currentPosts = blogData.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   // change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  const hasNextPage = Math.ceil(posts.length / postPerPage) > page;
+  console.log(hasNextPage);
+  const hasPreviousPage = page > 1;
 
   return (
     <>
@@ -21,7 +25,18 @@ export default function Home() {
         <title>Leberte blog</title>
       </Head>
       <Blogs blogData={currentPosts} />
-      <Pagination postPerPage={postPerPage} totalPost={blogData.length} paginate ={paginate} />
+      <Pagination page={page} hasNextPage={hasNextPage} hasPreviousPage={hasPreviousPage} postPerPage={postPerPage} totalPost={posts.length} paginate={paginate} />
     </>
   )
 }
+
+Home.getInitialProps = ({ query: { page = 1 } }) => {
+  const posts = blogData
+
+  return {
+    posts: posts,
+    page: +page
+  }
+}
+
+export default Home;
