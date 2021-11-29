c643d91d056695f1c91edce2f2dea377cf456275
Rakibur Rahman <rakibur5454@gmail.com>
2021-11-29 15:07:20 +0600

    navlinks and social icons read from another file

---
+++ data/NavLinks.js
@@ -0,0 +1,9 @@
+const NavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/', title: 'Feature' },
+  { href: '/', title: 'About' },
+  { href: '/', title: 'Contact' },
+  { href: '/', title: 'Pages' },
+]
+
+export default NavLinks;
