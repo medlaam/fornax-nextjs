a91161d6951b5f47ab38487db7653c18ea75bac2
Rakibur Rahman <rakibur5454@gmail.com>
2021-12-05 16:57:54 +0600

    added contact from with nodemailer

---
+++ pages/api/contact.js
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import nodemailer from 'nodemailer';
+export default async (req, res) => {
+  const { name, email, message, phone } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+
+  try {
+    const emailRes = await transporter.sendMail({
+      from: email,
+      to: 'rakibur5454@gmail.com',
+      subject: `Contact form submission from ${name}`,
+      html: `<p>You have a new contact form submission</p><br>
+      <p><strong>Name: </strong> ${name} </p><br>
+      <p><strong>Phone: </strong> ${phone} </p><br>
+      <p><strong>Message: </strong> ${message} </p><br>
+
+      `,
+    });
+
+    // console.log('Message Sent', emailRes);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(200).json(req.body);
+};
