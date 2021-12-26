e8b532bd2af1858a1c0439d02cf7ecc80ca5954c
Rakibur Rahman <rakibur5454@gmail.com>
2021-12-26 10:41:57 +0600

    reset input field and search component updated

--- components/search.js
+++ components/search.js
@@ -23,20 +23,21 @@ const Search = () => {
     }
   })
 
-  // const searchInput = () => {
-  //   setSearchTerm('')
-  // }
+  const resetSearchInput = () => {
+    setShowSearch(!showSearch)
+    setSearchTerm("");
+  }
 
 
   return (
     <div className={`${styles.searchContainer} container`}>
-      <div className=" overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-0 sm:top-20 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="large-modal">
+      <div className="fixed right-0 left-0 top-0 sm:top-20 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="large-modal">
         <div className={`relative px-4 w-full md:w-2/3 max-w-4xl h-full md:h-auto ${styles.searchModal}`}>
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex justify-between items-center rounded-t border-b dark:border-gray-600">
-              <input className="rounded focus:outline-none	w-full" type="text" placeholder="Search here" onChange={(e) => { setSearchTerm(e.target.value) }} />
+              <input className="rounded focus:outline-none	w-full" type="text" value={searchTerm} placeholder="Search here" onChange={(e) => { setSearchTerm(e.target.value) }} />
 
-              <button onClick={() => setShowSearch(!showSearch)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-3 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="large-modal">
+              <button onClick={resetSearchInput} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-3 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="large-modal">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
               </button>
             </div>
