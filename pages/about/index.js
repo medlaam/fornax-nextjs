afe1a784eb7353377137b36b41e81fcac60a3c00
Rakibur Rahman <rakibur5454@gmail.com>
2022-01-24 17:34:11 +0600

    update about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -18,7 +18,7 @@ const About = ({ about }) => {
           <div key={i} className={`mt-10 w-full p-4 md:w-2/4 md:p-0 m-auto  ${styles.about}`}>
             <div>
               <div className="relative w-full h-60">
-                <Image width={700} height={340} layout='fill' objectFit="cover" src={a.frontmatter.image}></Image>
+                <Image layout='fill' objectFit="cover" src={a.frontmatter.image}></Image>
               </div>
               <h3 className="mt-3 font-bold text-h3_sm md:text-h3">{a.frontmatter.name}</h3>
               <ul className={`flex my-5 ${styles.writersLink}`}>
