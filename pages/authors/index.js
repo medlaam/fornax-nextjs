fc9cf1792a805a9629511160168b2c1bcb95d39f
Rakibur Rahman <rakibur5454@gmail.com>
2022-01-26 17:39:03 +0600

    update author page

--- pages/authors/index.js
+++ pages/authors/index.js
@@ -19,16 +19,16 @@ const Authors = ({ authorsData, authorDefaultPage }) => {
       </Head>
       <div className={`mt-10 ${styles.about}`}>
         <h3 className="text-center text-textColor text-h3_sm md:text-h3">{title}</h3>
-        <div className="flex gap-4 flex-wrap my-10 justify-center m-auto">
+        <div className="flex gap-4 flex-wrap my-10 justify-center mx-auto">
           {
             authorsData.map((a, i) => (
-              <div key={i} className="p-4 shadow md:w-1/2 lg:w-1/3">
+              <div key={i} className="p-4 shadow w-2/3 max-w-xl">
                 <div className="text-center">
                   <Image height={100} width={100} objectFit="cover" src={a.frontmatter.image} />
                 </div>
                 <h5 className="mt-3 text-center font-bold text-textColor text-sm md:text-h5 hover:text-primaryColor"><Link href={`/authors/${kebabCase(a.frontmatter.name)}`}>{a.frontmatter.name}</Link></h5>
 
-                <div className="mt-4 text-center prose-p:text-sm" dangerouslySetInnerHTML={{ __html: marked.parse(a.content).slice(0, 150) }}>
+                <div className="mt-4 p-6 text-center prose-p:text-sm" dangerouslySetInnerHTML={{ __html: marked.parse(a.content).slice(0, 150) }}>
                 </div>
                 <ul className={`flex items-center justify-center mt-10 ${styles.writersLink}`}>
                   {
