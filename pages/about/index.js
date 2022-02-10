f53761b3ac04aaeefa32e088c6d710bb81c1904d
Rakibur Rahman <rakibur5454@gmail.com>
2022-02-10 12:14:29 +0600

    update about page image component

--- pages/about/index.js
+++ pages/about/index.js
@@ -18,7 +18,7 @@ const About = ({ about }) => {
           <div key={i} className={`py-16 px-4 sm:px-6 flex justify-center items-center ${styles.about}`}>
             <div className="mx-auto w-full max-w-4xl">
               <div className="relative w-full md:h-37 h-60">
-                <Image layout='fill' objectFit="cover" src={a.frontmatter.image}></Image>
+                <Image src={a.frontmatter.image} layout='fill' objectFit="cover" placeholder='blur' priority={true} blurDataURL='/9j/4AAQSkZJRgABAQAAAQABAAD/4gIoSUNDX1BST0ZJTEUAAQEAAAIYAAAAAAQwAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAAHRyWFlaAAABZAAAABRnWFlaAAABeAAAABRiWFlaAAABjAAAABRyVFJDAAABoAAAAChnVFJDAAABoAAAAChiVFJDAAABoAAAACh3dHB0AAAByAAAABRjcHJ0AAAB3AAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAFgAAAAcAHMAUgBHAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFhZWiAAAAAAAABvogAAOPUAAAOQWFlaIAAAAAAAAGKZAAC3hQAAGNpYWVogAAAAAAAAJKAAAA+EAAC2z3Bhcm'></Image>
               </div>
               <h3 className="mt-3 font-bold text-h3_sm md:text-h3">{a.frontmatter.name}</h3>
               <ul className={`flex my-5 ${styles.writersLink}`}>
